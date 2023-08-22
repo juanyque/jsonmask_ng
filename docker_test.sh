613781769a14f2a1e44e7451638ff40df2968009
Juan Garcia <juan.garcia@mobilife.es>
2023-08-22 20:59:58 +0200

    Add compatibility for Python 3.11
    Some warnings pending to fix

--- docker_test.sh
+++ docker_test.sh
@@ -5,3 +5,4 @@ docker-compose -f docker-compose_test-all.yml run --rm app_3.7 make test-all
 docker-compose -f docker-compose_test-all.yml run --rm app_3.8 make test-all
 docker-compose -f docker-compose_test-all.yml run --rm app_3.9 make test-all
 docker-compose -f docker-compose_test-all.yml run --rm app_3.10 make test-all
+docker-compose -f docker-compose_test-all.yml run --rm app_3.11 make test-all
